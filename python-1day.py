a596f0e75b687070150184741b17fec6d4b66f9c
bluebeaggle <shew1903@gmail.com>
2023-01-24 00:14:13 +0900

    day-1-2-class

--- python-1day.py
+++ python-1day.py
@@ -5,3 +5,10 @@ print("Hello" + "beaggle")
 print("hello", "beaggle")
 
 
+#Debuggin Practice
+print("Day1 - String Manipulation")
+print("String Concatenation is done with the + sing.")
+print('e.g print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+

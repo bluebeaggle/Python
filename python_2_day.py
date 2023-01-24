2431f728e88224a41cb53f5ef3bdb07387bcc575
bluebeaggle <shew1903@gmail.com>
2023-01-24 23:45:31 +0900

    Numbers

--- python_2_day.py
+++ python_2_day.py
@@ -15,3 +15,10 @@ print(3.14159)
 # True or False
 
 # Numbers
+num_char = len(input("What is your name?"))
+print(type(num_char))
+new_num_char = str(num_char)
+print(type(new_num_char))
+print("Your name has "+ new_num_char+ " charaters.")
+# str can not combine with int
+

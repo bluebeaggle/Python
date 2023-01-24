6db491cb2ac6ea3f750ad886b2613566c8278ca2
bluebeaggle <shew1903@gmail.com>
2023-01-24 22:23:07 +0900

    Data type_2

--- python_2_day.py
+++ python_2_day.py
@@ -2,3 +2,14 @@
 
 # String, Integer, Float, Boolean
 print("Hello"[0])
+print("Hello"[-1])
+
+# Integer
+print(1_000_000+2_000)
+
+# Float
+print(1_000.0)
+print(3.14159)
+
+# Boolean
+# True or False

4e292f3804fbfda400451f924d9b02ff70076d2c
bluebeaggle <shew1903@gmail.com>
2023-01-26 00:56:15 +0900

    f-string

--- python_2_day.py
+++ python_2_day.py
@@ -32,5 +32,20 @@ print("100/3  :",100/3,"(auto float type translate)")
 print("100//3 :",100//3)
 print("100%3 :",100%3)
 print("순서 : 괄호 > 지수(**) > 곱셈,나눗셈(*,/) > 덧셈,뺄셈(+,-)")
+
+
+# mathmatic 
+print("8/3 =", 8/3)
+print("int(8/3) = ",int(8/3))
+print("round(8/3) = ", round(8/3))
+print("round(8/3, 2) = ", round(8/3, 2), "소수점 3번째에서 반올림")
 print("------------------------")
+
+# F-string
+score = 0
+height = 1.8
+iswinning = True
+print(f"your score is {score} (int), your height is {height}(float), you are winning is {iswinning}(boolean)")
+print("f-String은 {}안에 변수를 넣으면 되며, 다른 Data type을 문자열로 처리하는 작업이 수월하다.")
+
 

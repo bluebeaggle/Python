5f9f3662271df8cea422c1ce77675cd6bd99dc31
bluebeaggle <shew1903@gmail.com>
2023-02-05 22:10:39 +0900

    python_4day exercise 1"

--- python_4day_class1.py
+++ python_4day_class1.py
@@ -1,7 +1,13 @@
 import random
 import python_4day_mymodule
 
-random_interger = random.randint(1,10)
+random_interger = random.randint(1,10)  #1 and 10 (include)
 print(random_interger)
 
 print(python_4day_mymodule.pi)
+
+random_float = random.random() # 0 and 1 (exclude)
+print(random_float)
+
+love_score = random.randint(1,100)
+print(f"Your love score is {love_score}")

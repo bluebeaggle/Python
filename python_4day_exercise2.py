bebd0609960cc8302058ba960f6ae072f1d0a751
bluebeaggle <shew1903@gmail.com>
2023-02-05 22:41:27 +0900

    Bank Roulette_modify

--- python_4day_exercise2.py
+++ python_4day_exercise2.py
@@ -15,4 +15,6 @@ random_name = random.randint(0,names_number-1)
 
 print(f"{names[random_name]} is going to buy the meal today!")
 
+person = random.choice(names)
+print(person + " is going to buy the meal today!")
 

6b3b2baccb232566874b5d44d1e0e782cc416e91
bluebeaggle <shew1903@gmail.com>
2023-02-05 22:37:12 +0900

    Bank Roulette

--- python_4day_exercise2.py
+++ python_4day_exercise2.py
@@ -1,2 +1,18 @@
 # Banker Roulette
 
+#Import the random module here
+
+#Split string method
+names_string = input("Give me everybody's names, separated by a comma.")
+names = names_string.split(",")
+#Don't change the code above
+
+#Write your code below this line
+
+import random
+names_number = len(names)
+random_name = random.randint(0,names_number-1)
+
+print(f"{names[random_name]} is going to buy the meal today!")
+
+

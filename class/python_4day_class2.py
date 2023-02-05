0185b033dc43eb508ef02f1d94b73c4cfefaf7aa
bluebeaggle <shew1903@gmail.com>
2023-02-05 22:25:32 +0900

    python 4day class 2

---
+++ class/python_4day_class2.py
@@ -0,0 +1,25 @@
+#list
+
+state1 = "Delaware"
+state2 = "pennsylvania"
+
+state_of_america = ["Delaware", "Pennsylvania","New Jersy","Georgia", "Connecvticut", "Massachusetts", "maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tenessee", "Ohio", "Louisiana", "Indiana", "Mississippi","Illinois","Alabama", "Maine", "Missouri", "Arkansas", "West Virginia", "Texas", "Iowa", "Wisconsin", "Calinfornia","Minnesota", "Oregon", "Kansas", "Florida" ]
+
+print(state_of_america)
+
+#list.append(x)
+
+#list.extend(iterable)
+
+#list.insert(i,x)
+
+#list.pop([i])
+
+#list.clear()
+
+#list.sort()
+
+#list.remove(x)
+
+
+

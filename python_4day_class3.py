28523006cf5915e745536b356e5e3b47be81a156
bluebeaggle <shew1903@gmail.com>
2023-02-07 08:39:45 +0900

    python_4class

---
+++ python_4day_class3.py
@@ -0,0 +1,21 @@
+import random
+
+
+
+states_of_america = ['Delaware', 'Pennsylvania', 'New Jersey', 'Georgia', 'Connecticut', 'Massachusetts', 'Maryland', 'South Carolina', 'New Hampshire', 'Virginia', 'New York', 'North Carolina', 'Rhode Island', 'Vermont', 'Kentucky', 'Tennessee', 'Ohio', 'Louisiana', 'Indiana', 'Mississippi', 'Illinois', 'Alabama', 'Maine', 'Missouri', 'Arkansas', 'Michigan', 'Florida', 'Texas', 'Iowa', 'Wisconsin', 'California', 'Minnesota', 'Oregon', 'Kansas', 'West Virginia', 'Nevada', 'Nebraska', 'Colorado', 'North Dakota', 'South Dakota', 'Montana', 'Washington', 'Idaho', 'Wyoming', 'Utah', 'Oklahoma', 'New Mexico', 'Arizona', 'Alaska', 'Hawaii']
+
+
+print(states_of_america[49])
+
+
+dirt_dozen = ["Stawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"]
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables= ["Spinach", "Kale", "Tomatoes", "Celery", "Potates"]
+
+dirt_dozen2 = [fruits, vegetables]
+
+print(dirt_dozen2) #2중 리스트
+
+print(dirt_dozen2[1][1])
+
+

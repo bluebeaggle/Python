23704fe201de0229d5fa8f2ad9c50b18ab18cd50
bluebeaggle <shew1903@gmail.com>
2023-02-23 21:53:42 +0900

    6day class

---
+++ class/6day_class.py
@@ -0,0 +1,23 @@
+# Functions
+# docs.python.org/3/library/functions.html
+# Built-in Function
+
+print("Hello")
+num_char = len("Hello")
+print(num_char)
+
+# Make own function
+def my_function() :
+    print("This is my function! Not your function!")
+
+my_function()
+# Instance
+hello = my_function
+hello()
+
+
+
+
+
+# While Loops
+

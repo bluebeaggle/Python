dff7a34b9641323dc245c8c41f59af63161a3bac
bluebeaggle <shew1903@gmail.com>
2023-02-26 18:24:48 +0900

    update hanman

--- 7day_hangman.py
+++ 7day_hangman.py
@@ -1,47 +1,63 @@
 import random
 word_list=["ardvark", "baboon", "camel"]
 
-game_start = random.choice(word_list)
-hangman=[]
-for i in game_start :
-    hangman.append(i)
-question = []
-for i in range(len(hangman)) :
-    question.append("- ")
+# game_start = random.choice(word_list)
+# hangman=[]
+# for i in game_start :
+#     hangman.append(i)
+# question = []
+# for i in range(len(hangman)) :
+#     question.append("- ")
 
-print(hangman)
-print(question)
+# print(hangman)
+# print(question)
 
-life = 5
+# life = 5
 
-while(True) :
-    customer_input=input("Guess ? \n")
-    if customer_input in hangman :
-        for i in range(len(hangman)) :
-            if customer_input is hangman[i] :
-                question[i] = customer_input
-    else :
-        life -=1
-        if life == 0:
-            print(f"Your life is {life}")
-            print("You lose")
-            break
-    if "- " not in question :
-        print(question)
-        print("Success!")
-        break
-    print(life)
-    print(question)
+# while(True) :
+#     customer_input=input("Guess ? \n")
+#     if customer_input in hangman :
+#         for i in range(len(hangman)) :
+#             if customer_input is hangman[i] :
+#                 question[i] = customer_input
+#     else :
+#         life -=1
+#         if life == 0:
+#             print(f"Your life is {life}")
+#             print("You lose")
+#             break
+#     if "- " not in question :
+#         print(question)
+#         print("Success!")
+#         break
+#     print(life)
+#     print(question)
+    
+    
+    
     
 chosen_word = random.choice(word_list)
 display = []
 for letter in chosen_word :
     display += '_'
-    
-guess = input("Geuss a letter : ").lower()
-for letter in chosen_word:
-    if letter == guess :
-        print("Right")
+
+print(display)
+
+
+
+def question() :
+    guess = input("Geuss a letter : ").lower()
+    for i in range(len(chosen_word)) :
+        letter = chosen_word[i]
+        if letter == guess :
+            display[i] = letter
+    print(display)
+
+while(True) :
+    if '_' in display :
+        question()
     else :
-        print("Wrong")
+        print("You win!")
+        break
+
 

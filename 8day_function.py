df5a59789833befbf18d77eb3ddc91c444fea39f
bluebeaggle <shew1903@gmail.com>
2023-02-26 19:26:33 +0900

    8day start

---
+++ 8day_function.py
@@ -0,0 +1,12 @@
+def greet() :
+    print("hello")
+    print("hello")
+    print("hello")
+greet()
+
+def greet_with_name(name) :
+    print(f"hello {name} ")
+    print(f"hello {name} ")
+    print(f"hello {name} ")
+greet_with_name("Daeheon")    
+

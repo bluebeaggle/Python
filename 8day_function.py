e8d4b2a80ec1d50a1aa2ec59dc6f3f17363ff486
bluebeaggle <shew1903@gmail.com>
2023-02-26 19:31:44 +0900

    8day_class

--- 8day_function.py
+++ 8day_function.py
@@ -10,3 +10,8 @@ def greet_with_name(name) :
     print(f"hello {name} ")
 greet_with_name("Daeheon")    
 
+def greet_with(name, location) :
+    print(f'hello my name is {name} and I live in {location}')
+greet_with("Daeheon", "Seoul")
+greet_with(location = "Seoul", name="Daeheon")
+
